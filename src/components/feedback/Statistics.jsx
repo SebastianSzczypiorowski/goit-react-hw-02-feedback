cbe0bf2ff76ddb1fdd1826a0bc46a9f863aa110f
SebastianSzczypiorowski <110854817+SebastianSzczypiorowski@users.noreply.github.com>
2023-03-26 18:46:10 +0200

    1

--- src/components/feedback/Statistics.jsx
+++ src/components/feedback/Statistics.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Notification from './Notification';
-
+// 1
 
 const Statistics = ({ good, neutral, bad, total, positivePercentage}) => {
   return (
